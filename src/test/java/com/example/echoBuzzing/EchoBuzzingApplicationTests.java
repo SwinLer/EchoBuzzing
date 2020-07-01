4a6e25a97bae3870579eacbd928fd0be86943f43
lijiaxin <1220494758@qq.com>
2020-07-01 21:24:18 +0800

    finish work

---
+++ src/test/java/com/example/echoBuzzing/EchoBuzzingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.echoBuzzing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoBuzzingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
